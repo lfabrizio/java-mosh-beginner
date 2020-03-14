e577841e4ed0963c7597e9e022bce5b2639812dc
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-14 17:32:09 -0400

    strings

---
+++ 01-getting-started/src/com/fabrizio/strings.java
@@ -0,0 +1,11 @@
+package com.fabrizio;
+
+public class strings {
+
+    public static void main(String[] args){
+        String message = "Hello World" + "!!";
+
+        System.out.println(message.toLowerCase());
+        System.out.println(message);
+    }
+}
