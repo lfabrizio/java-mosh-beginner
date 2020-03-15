b6f0593f177f4e26401ceb587b0e3e5fb31859be
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-15 16:35:07 -0400

    multarrays

---
+++ 01-getting-started/src/com/fabrizio/multiarrays.java
@@ -0,0 +1,12 @@
+package com.fabrizio;
+
+import java.util.Arrays;
+
+public class multiarrays {
+    public static void main(String[] args){
+        int[][] numbers = { { 1, 2, 3 }, { 4, 5, 6 } };
+        
+        System.out.println(Arrays.deepToString(numbers));
+
+    }
+}
