ae3a349fdc884a225f0df732fc61147029e7a5bf
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-15 16:46:54 -0400

    constants

---
+++ 01-getting-started/src/com/fabrizio/constants.java
@@ -0,0 +1,8 @@
+package com.fabrizio;
+
+public class constants {
+
+    public static void main(String[] args){
+        final float pi = 3.14F;
+    }
+}
--- 01-getting-started/src/com/fabrizio/multiarrays.java
+++ 01-getting-started/src/com/fabrizio/multiarrays.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class multiarrays {
     public static void main(String[] args){
         int[][] numbers = { { 1, 2, 3 }, { 4, 5, 6 } };
-        
+
         System.out.println(Arrays.deepToString(numbers));
 
     }
