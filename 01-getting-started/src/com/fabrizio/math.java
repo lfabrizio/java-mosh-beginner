0e94e5abacdaaa4aa4abbe57f1be76160e265734
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-16 16:52:58 -0400

    math

---
+++ 01-getting-started/src/com/fabrizio/math.java
@@ -0,0 +1,8 @@
+package com.fabrizio;
+
+public class math {
+    public static void main(String[] args){
+        int result = (int) (Math.random() * 100);
+        System.out.println(result);
+    }
+}
