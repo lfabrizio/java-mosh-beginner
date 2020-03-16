30c5eb879b6218cb323a827171b1a2e386c9bc53
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-16 19:01:49 -0400

    add folder

---
+++ 01-getting-started/src/com/fabrizio/motrgagecalc.java
@@ -0,0 +1,32 @@
+package com.fabrizio;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class motrgagecalc {
+    public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR    = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.println("Annual interest rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.println("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                        * (monthlyInterest * Math.pow(1 + monthlyInterest)
+                        / (Math.pow(1 + monthlyInterest, numberOfPayments))
+
+                String mortgageFormatted = NumberFormat.getCurrencyInstance()
+                System.out.println("Mortgage: " + mortgageFormatted);
+
+    }
+}
