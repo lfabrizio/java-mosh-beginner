5fecd42abbd019cb7c0d198521d9d3bb1c715f32
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-17 19:13:47 -0400

    if statements

---
+++ 03-Control-Flow/src/com/fabrizio/ifconditions.java
@@ -0,0 +1,15 @@
+package com.fabrizio;
+
+public class ifconditions {
+    int temp = 32;
+    if(temp >30)
+
+    {
+        System.out.println("It's a hot day");
+        System.out.println("Drink Water");
+    } else if(temp >20&&temp <=30) {
+        System.out.println("beautiful day");
+    } else {
+        System.out.println("cold day");
+    }
+}
