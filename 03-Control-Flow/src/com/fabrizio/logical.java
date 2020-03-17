e5f18ed41c7590d303d14b35c8660032a5d13deb
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-17 18:51:12 -0400

    logical

---
+++ 03-Control-Flow/src/com/fabrizio/logical.java
@@ -0,0 +1,11 @@
+package com.fabrizio;
+
+public class logical {
+
+    public static void main(String[] args) {
+        boolean hasHighIncome = false;
+        boolean hasGoodCredit = true;
+        // boolean hasCriminalRecord = true;
+        boolean isEligible = hasHighIncome || hasGoodCredit;
+    }
+}
