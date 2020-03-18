5c6b605141118a207b9be932fd0093df7f019f0a
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-18 18:07:56 -0400

    forloop

---
+++ 03-Control-Flow/src/com/fabrizio/forloops.java
@@ -0,0 +1,8 @@
+package com.fabrizio;
+
+public class forloops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++)
+            System.out.println("Hello World");
+    }
+}
