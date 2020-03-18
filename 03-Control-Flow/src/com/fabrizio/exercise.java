1d9e8d39b34505b78a7063432cb6e8222557e04e
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-18 18:04:33 -0400

    exercise

---
+++ 03-Control-Flow/src/com/fabrizio/exercise.java
@@ -0,0 +1,21 @@
+package com.fabrizio;
+
+import java.util.Scanner;
+
+public class exercise {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Number: ");
+        int number = scanner.nextInt();
+
+        if (number % 5 == 0 && number % 3 == 0)
+            System.out.println("fizz");
+        else if (number % 3 == 0)
+            System.out.println("Buzz");
+        else if (number % 5 == 0)
+            System.out.println("FizzBuzz");
+        else
+            System.out.println(number);
+    }
+
+}
--- 03-Control-Flow/src/com/fabrizio/switchs.java
+++ 03-Control-Flow/src/com/fabrizio/switchs.java
@@ -3,19 +3,7 @@ package com.fabrizio;
 public class switchs {
     public static void main(String[] args) {
         String role = "admin";
-
-        switch (role) {
-            case "admin":
-                System.out.println("You're an admin");
-                break;
-
-            case "moderator": {
-                System.out.println("You're a moderator");
-                break;
-
-                default: {
-                    System.out.println("You're a guest");
-                }
-            }
-        }
+        if (role == "admin")
+            System.out.println("You're an admin");
     }
+}
