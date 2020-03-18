c9f693444501b784bc6d5c38eea1a00d4974a1a8
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-18 17:42:15 -0400

    switch

---
+++ 03-Control-Flow/src/com/fabrizio/switchs.java
@@ -0,0 +1,21 @@
+package com.fabrizio;
+
+public class switchs {
+    public static void main(String[] args) {
+        String role = "admin";
+
+        switch (role) {
+            case "admin":
+                System.out.println("You're an admin");
+                break;
+
+            case "moderator": {
+                System.out.println("You're a moderator");
+                break;
+
+                default: {
+                    System.out.println("You're a guest");
+                }
+            }
+        }
+    }
