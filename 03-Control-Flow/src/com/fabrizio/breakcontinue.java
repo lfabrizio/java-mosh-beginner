365f936d304e7c91ea8d04132a248f87854d2a84
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-19 17:33:27 -0400

    breakcontinue

---
+++ 03-Control-Flow/src/com/fabrizio/breakcontinue.java
@@ -0,0 +1,21 @@
+package com.fabrizio;
+
+import java.util.Scanner;
+
+public class breakcontinue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (true) {
+            System.out.println("Input: ");
+            input = scanner .next().toLowerCase();
+            if (!input.equals("pass"));
+            continue;
+            if (input.equals("pass"))
+                continue;
+            if (input.equals("quit"))
+                break;
+            System.out.println(input);
+        }
+    }
+}
