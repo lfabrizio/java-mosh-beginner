1fec53a0c6ec394a1deb396e8ecc691bc64da02d
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-19 18:37:49 -0400

    dowhile

--- 03-Control-Flow/src/com/fabrizio/dowhile.java
+++ 03-Control-Flow/src/com/fabrizio/dowhile.java
@@ -1,17 +1,17 @@
-package com.fabrizio;
-
-import java.util.Scanner;
-
-public class dowhile {
-
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            String input = "";
-            do {
-                System.out.println("INput: ");
-                input = scanner .next().toLowerCase();
-                System.out.println(input);
-            }  while (!input.equals("quit"));
-            }
-    }
+//package com.fabrizio;
+//
+//import java.util.Scanner;
+//
+//public class DoWhile {
+//
+//        public static void main(String[] args) {
+//            Scanner scanner = new Scanner(System.in);
+//            String input = "";
+//            do {
+//                System.out.println("Input: ");
+//                input = scanner.next().toLowerCase();
+//                System.out.println(input);
+//            }  while (!input.equals("quit"));
+//        }
+//    }
 
---
+++ 03-Control-Flow/src/com/fabrizio/foreach.java
@@ -0,0 +1,11 @@
+package com.fabrizio;
+
+public class foreach {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Mango", "Orange"};
+
+        for (int i = fruits.length; i > 0; i--)
+            System.out.println(fruits[i]);
+
+    }
+    }
