6ff32930f97a8e0fa6e6a4e47813c16fc8be3d28
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-20 18:35:57 -0400

    mortgage calc

---
+++ 03-Control-Flow/src/com/fabrizio/mortgageclass.java
@@ -0,0 +1,48 @@
+package com.fabrizio;
+
+import java.util.Scanner;
+
+public class Mortgageclass {
+    public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        int principal = 0;
+        float monthlyInterest = 0;
+        int numberOfPayments = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Principal: ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("enter a value: ");
+        }
+
+        while (true) {
+            System.out.println("Annual Interest rate: ");
+            float annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest <= 30) {
+                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("enter a value: ");
+
+        }
+        while (true) {
+            System.out.println("Period (Years): ");
+            byte years = scanner.nextByte();
+            if (years >= 1 && years <= 30) {
+                numberOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("error message: ");
+        }
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest));
+    }
+
+
