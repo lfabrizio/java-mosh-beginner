6946bf454ee733fa0bd0e0b6c08b3e2112c7b45e
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-21 15:54:06 -0400

    methods

--- 04-Clean-Coding/src/com/fabrizio/Main.java
+++ 04-Clean-Coding/src/com/fabrizio/Main.java
@@ -3,6 +3,10 @@ package com.fabrizio;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    greetUser("Mosh", "Smith");
+    }
+
+    public static void greetUser(String firstName, String lastName){
+        System.out.println("Hello " + firstName +  " " + lastName);
     }
 }
