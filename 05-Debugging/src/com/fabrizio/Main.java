81a4c1c4886cc37ddd366be7699e708c15c6ee1c
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-22 15:57:21 -0400

    debug

--- 05-Debugging/src/com/fabrizio/Main.java
+++ 05-Debugging/src/com/fabrizio/Main.java
@@ -3,7 +3,13 @@ package com.fabrizio;
 public class Main {
 
     public static void main(String[] args) {
-	    int i = 0;
-        if (i == 2)
+        System.out.println("start");
+        printNumbers( 4);
+        System.out.println("finish");
+    }
+
+    public static void printNumbers(int limit) {
+        for (int i = 0; i < limit; i += 2)
+            System.out.println(i);
     }
 }
